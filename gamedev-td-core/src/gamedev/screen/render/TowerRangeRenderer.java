47c5ca96df142939222b5e3a0a5750d82325a830
baseballbear <rbecruz@gmail.com>
2014-12-14 20:29:25 +0800

    Fixed drawing of the tower range.

--- gamedev-td-core/src/gamedev/screen/render/TowerRangeRenderer.java
+++ gamedev-td-core/src/gamedev/screen/render/TowerRangeRenderer.java
@@ -2,6 +2,9 @@ package gamedev.screen.render;
 
 import gamedev.entity.Tower;
 import gamedev.td.Config;
+import gamedev.td.helper.MathHelper;
+
+import java.awt.Point;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -28,9 +31,11 @@ public class TowerRangeRenderer extends ShapeRenderer {
 		begin(ShapeType.Filled);
 		// TODO: Fix this code; it's ugly. What are you trying to do?
 		
-		if(towerToBuild != null)
-			circle(towerToBuild.getX() + Config.tileSize / 2, convertYforShapeRenderer(towerToBuild.getY() + Config.tileSize * 3 / 2), attackRange);
+		if(towerToBuild != null){
+			Point convertedCoordinates = MathHelper.convertToGrid(towerToBuild.getX(), towerToBuild.getY());
+			circle(convertedCoordinates.x + Config.tileSize / 2, convertYforShapeRenderer(convertedCoordinates.y + Config.tileSize * 3 / 2), attackRange);
 		
+		}
 		// if (towerToBuild != null) {
 		// circle(towerToBuild.getX() + Config.tileSize / 2, convertYforShapeRenderer(towerToBuild.getY() + Config.tileSize * 3 / 2), rangeRadius);
 		// } else if (towerClone != null) {
--- gamedev-td-core/src/gamedev/td/helper/MathHelper.java
+++ gamedev-td-core/src/gamedev/td/helper/MathHelper.java
@@ -1,5 +1,7 @@
 package gamedev.td.helper;
 
+import gamedev.td.Config;
+
 import java.awt.Point;
 
 import com.badlogic.gdx.math.Vector2;
@@ -7,5 +9,11 @@ import com.badlogic.gdx.math.Vector2;
 public class MathHelper {
 	public static Vector2 PointToVector2(Point p){
 		return new Vector2(p.x, p.y);
+	}
+	
+	public static Point convertToGrid(float x, float y){
+		int newX = (int) x / Config.tileSize * Config.tileSize;
+		int newY = (int) y / Config.tileSize * Config.tileSize;
+		return new Point(newX, newY);
 	}
 }
